1579ad01b9f6d22574bb84ac57fcb89cf2dea2d8
Peter Boling <peter.boling@gmail.com>
2025-02-25 00:46:08 +0700

    🐛 Ruby < 3.0 compat: Bifurcate the Modern Double Splat (tiga)

--- lib/appraisal.rb
+++ lib/appraisal.rb
@@ -1,10 +1,12 @@
 # frozen_string_literal: true
 
-require "appraisal/version"
-require "appraisal/task"
-
+# Define the namespace of this library!
 module Appraisal
+  # Define constants that should be available throughout the library.
   MODERN_DOUBLE_SPLAT = Gem::Version.create(RUBY_VERSION) >= Gem::Version.create('3.0')
 end
+
+require "appraisal/version"
+require "appraisal/task"
 
 Appraisal::Task.new
